b25e182098108e16100b5ad73e9f0d5acdf36af6
qijun1900 <3308837335@qq.com>
2025-04-14 00:08:06 +0800

    none

--- node/routes/admin/ExamRouter.js
+++ node/routes/admin/ExamRouter.js
@@ -4,8 +4,6 @@ var ExamRouter = express.Router();
 const multer  = require('multer')
 const upload = multer({ dest: 'public/examcoveruploads/' })
 
-
-
 //涉及文件上传
 ExamRouter.post('/adminapi/exam/add',upload.single('file'),ExamController.add)
 
