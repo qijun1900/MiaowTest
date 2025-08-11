c34ae91f3f633bac5e4abfafd7535e950820452e
qijun1900 <3308837335@qq.com>
2025-08-11 16:06:11 +0800

    恢复开发环境

--- node/db/db.js
+++ node/db/db.js
@@ -11,13 +11,13 @@ const paneldbconfig = require('../config/1panelmongo.config')
 
     
 //本地连接 mongodb 服务 
-// mongoose.connect(`mongodb://${config.DBHOST}:${config.DBPORT}/${config.DBNAME}`);//不存在自动创建
+mongoose.connect(`mongodb://${config.DBHOST}:${config.DBPORT}/${config.DBNAME}`);//不存在自动创建
 
 // 1panel 连接mongodb数据库
 // mongoose.connect('mongodb://username:password@host:port/database', {
 //     authSource: 'admin' // 关键：指定认证数据库为 admin
 //   });
-mongoose.connect(`mongodb://${paneldbconfig.username}:${paneldbconfig.password}@${paneldbconfig.host}:${paneldbconfig.sport}/${paneldbconfig.databasename}`,{authSource: 'admin'})
+// mongoose.connect(`mongodb://${paneldbconfig.username}:${paneldbconfig.password}@${paneldbconfig.host}:${paneldbconfig.sport}/${paneldbconfig.databasename}`,{authSource: 'admin'})
 
 //4.设置回调, 
 mongoose.connection.once('open',()=>{
