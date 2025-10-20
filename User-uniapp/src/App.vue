36a21f0e402b2d36424ff11138ee121dbb4dc48c
qijun1900 <3308837335@qq.com>
2025-10-20 21:24:37 +0800

    恢复文件

---
+++ User-uniapp/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    onLaunch: function() {
+	//#ifdef MP-WEIXIN
+      wx.cloud.init({
+		env: 'prod-4gx4oupb178049a1'
+	});
+	//#endif
+	},
+	onShow: function() {
+	
+	},
+	onHide: function() {
+		
+	}
+}
+</script>
+
+<style>
+/*每个页面公共css */
+uni-modal{
+  z-index: 19999 !important;
+}
+</style>
---
+++ User-uniapp/src/config/esc.config.js
@@ -0,0 +1,14 @@
+const escconfig = {
+  serverHost: "47.95.37.174", // 服务器地址
+  serverPort: 3001,// 服务器端口
+  tunnelUrl: "https://1142zt884zj05.vicp.fun", // 内网穿透地址
+  useTunnel:false, // 是否使用内网穿透地址
+  //#ifdef MP-WEIXIN
+  useCloudContainer: true, // 是否启用微信云托管
+  cloudEnv: "prod-4gx4oupb178049a1", // 云托管环境ID
+  cloudService: "miaowtest" // 云托管服务名
+  //#endif
+  
+};
+// 使用 ES6 标准导出语法
+export default escconfig;
