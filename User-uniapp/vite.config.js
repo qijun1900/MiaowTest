d44f323141c53635509d8544ff4b871079fb2f5b
qijun1900 <3308837335@qq.com>
2025-10-24 20:56:21 +0800

    添加环境变量

--- User-uniapp/vite.config.js
+++ User-uniapp/vite.config.js
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    uni(),
-    visualizer()
-  ],
-  define: {
-    'process.env': process.env
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // 取消sass废弃API的报警
-        silenceDeprecations: ['legacy-js-api', 'color-functions', 'import'],
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      uni(),
+      visualizer()
+    ],
+    define: {
+      'process.env': process.env
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // 取消sass废弃API的报警
+          silenceDeprecations: ['legacy-js-api', 'color-functions', 'import'],
+        },
       },
     },
-  },
-  server: {
-    port: 5050,//启动端口
-    open: true, // 启动时自动打开浏览器
+    server: {
+      port: 5050,//启动端口
+      open: true, // 启动时自动打开浏览器
+    }
   }
 })
